2e2025a9bc0d08a4df806ad5bc1a31bc8f26e45a
Karsten <karsten.saetre@gmail.com>
2017-05-30 00:33:54 +0200

    Refactor User component html into HTML dedicated file

--- src/app/components/user.component.ts
+++ src/app/components/user.component.ts
@@ -4,46 +4,8 @@ import {JsonService} from '../services/json.service';
 @Component({
     moduleId: module.id, //For using relative paths
     selector: 'user',
-    template: `
-    <h1>Hello {{name}}</h1>
-    <p><Strong>Email:</Strong> {{email}}</p>
-    <p><Strong>Address:</Strong>{{address.street}} {{address.city}}, {{address.state}}</p>
-    <button (click)="toggleHobbies()">{{showHobbies ? "Hide Hobbies" : "Show Hobbies"}}</button>
-    <div *ngIf="showHobbies">
-        <h3>Hobbies</h3>
-            <ul>
-                <li *ngFor="let hobby of hobbies; let i = index">
-                    {{hobby}} <button (click)="deleteHobby()">X</button>
-                </li>
-            </ul>
-            <form (submit)="addHobby(hobby.value)">
-                <label>Add Hobby: </label><br />
-                <input type="text" #hobby /><br />
-            </form>
-    </div>
-    <hr />
-    <h3>Edit User</h3>
-    <form>
-        <label>Name: </label><br />
-        <input type="text" name="name" [(ngModel)]="name" /><br />
-        <label>Email: </label><br />
-        <input type="text" name="email" [(ngModel)]="name" /><br />
-        <label>Street: </label><br />
-        <input type="text" name="address.street" [(ngModel)]="address.street" /><br />
-        <label>City: </label><br />
-        <input type="text" name="address.city" [(ngModel)]="address.city" /><br />
-        <label>State: </label><br />
-        <input type="text" name="address.state" [(ngModel)]="address.state" /><br />
-    </form>
-    <hr />
-    <h3>Jsons</h3>
-    <div *ngFor="let json of jsons">
-        <h3>{{json.title}}</h3>
-        <p>{{json.body}}</p>
-    `,
     templateUrl: 'user.component.html',
     providers: [JsonService]
-    // styles: ['div {width: 400px;  margin: 0px auto;}']
 })
 export class UserComponent  {
     name: string;
@@ -67,6 +29,7 @@ export class UserComponent  {
         this.hobbies = ['Tea', 'Movies', 'Gaming'];
         this.showHobbies = false;
 
+        // Gets json results
         this.jsonService.getJson().subscribe(jsons => {
             // console.log(jsons);
             this.jsons = jsons;
@@ -78,12 +41,12 @@ export class UserComponent  {
         this.showHobbies = !this.showHobbies;
     }
 
-    addHobby(hobby){
+    addHobby(hobby: any){
         console.log(hobby);
         this.hobbies.push(hobby);
     }
 
-    deleteHobby(i){
+    deleteHobby(i: any){
         this.hobbies.splice(i, 1);
     }
 
